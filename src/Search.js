1e028d32bbe78c99cb57588a4cc96fbffaf986ea
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-03-29 08:27:32 -0400

    building out search functionality

--- src/Search.js
+++ src/Search.js
@@ -1,31 +1,18 @@
 import React, {Component} from "react"
-import styles from './styles/index'
 
 class Search extends Component {
   render(){
     let {handleSearchInput, handleSubmitQuery, query} = this.props
     return (
-      <div style={styles.transparentBg} className="jumbotron col-sm-6 col-sm-offset-3 text-center">
-        <div className="col-sm-12">
-          <form onSubmit={(evt) => handleSubmitQuery(evt)}>
-            <div className="form-group">
-              <input
-                onChange={(evt) => handleSearchInput(evt)}
-                value={query}
-                className="form-control"
-                type="text"
-                placeholder="Enter a Movie Title..." />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-primary"
-                type="submit">Search</button>
-            </div>
+      <div>
+          <form onSubmit={(event) => this.props.submitQuery(event)}>
+              <input type "text" placeholder="Search for a movie dawg!" value {this.props.query} onChange={(e)} => this.props.handleSearch(e)} />
+              <button type="submit" > Search </button>
           </form>
         </div>
-      </div>
-    )
+      )
+    }
+
   }
-}
 
 export default Search
